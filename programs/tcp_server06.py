4a8544bc59cc443a3df966f1cf5541eb916c95b2
Ram Rustagi <rprustagi@smartron.com>
2020-03-22 15:58:52 +0530

    removed hard coding of constatns

--- programs/tcp_server06.py
+++ programs/tcp_server06.py
@@ -8,11 +8,14 @@ parser = argparse.ArgumentParser(description="Simple Server for N/w Delays")
 parser.add_argument('-s', '--server', type=str, default="0.0.0.0")
 parser.add_argument('-p', '--port', type=int, default=9999)
 parser.add_argument('-b', '--buffer',  type=int, default=1000)
+parser.add_argument('-c', '--cnt_children',  type=int, default=10)
 args = parser.parse_args()
 
 ip_addr = args.server
 port = args.port
 buffer = args.buffer
+cnt_children = args.cnt_children
+
 
 # ----------------------------------------------
 def child():
@@ -30,7 +33,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 srvr_addr = (ip_addr, port)
 sock.bind(srvr_addr)
 sock.listen(1)
-for ii in range(10): # create predefined number of child processes
+for ii in range(cnt_children): # create predefined number of child processes
   childpid = os.fork()
   if childpid == 0:
     child()
