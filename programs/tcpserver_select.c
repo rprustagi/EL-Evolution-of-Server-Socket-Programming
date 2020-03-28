556df9a47fc375f916964f15539ddbaf4376f1b6
Ram Rustagi <rprustagi@smartron.com>
2020-03-28 16:15:07 +0530

    correct header files and server config parameters

--- programs/tcpserver_select.c
+++ programs/tcpserver_select.c
@@ -1,7 +1,12 @@
+/**
+ * server uses select() to deal with multiple clients.
+ * it doesn not create a new process/thread for each client.
+ */
+
 #include <netdb.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <strings.h>
+#include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <signal.h>
@@ -10,110 +15,151 @@
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <sys/time.h>
+
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-/* server side parameters */
-#define SERVER_PORT	12345
-#define LISTENQ     2         /* 2nd argument to listen () */
-#define BUFFSIZE    8192         /* buffer size for reads and writes */
+/* server configuration parameters */
+#define SERVER_PORT     12345
+#define LISTENQ         2         /* 2nd argument to listen () */
+#define BUFFSIZE        8192      /* buffer size for reads and writes */
 
-#define	MAX_CLIENTS	1000
-#define	SELECT_TIMEOUT 10
-#define SERVERPORT 9999
+#define  MAX_CLIENTS    1000
+#define  SELECT_TIMEOUT 10
+
 int str_echo(int fd);
 
 int main(int argc, char **argv) {
-	int     listenfd, connfd;
-	struct sockaddr_in cliaddr, servaddr;
-	socklen_t clilen;
+  int     listenfd, connfd;
+  pid_t   childpid;
+  struct sockaddr_in cliaddr, servaddr;
+  socklen_t clilen;
+  int  child_status;
+  int opt;
+  char *ip_addr = (char *) NULL;
+  short port = 0;
+  fd_set  rset; /* master read fdset */
+  fd_set  wset; /* master write fdset */
+  fd_set  wk_rset; /* working read set */
+  fd_set  wk_wset; /* working write set */
+  struct timeval timeout;
 
-	fd_set	rset, wset; /* master read and write fdset */
-	fd_set	wk_rset, wk_wset; /* working read and write fdset */
-	struct timeval timeout = {10, 0};
+  int max_fd; /* max socket fd value */
+  int client_sock[MAX_CLIENTS];
+  int ifd; /* fd of current interest */
+  int  ii; /* index variable */
+  int cnt;  /* count of sockets of interest at an invocation */
+  int status;
 
-	int max_fd; /* max socket fd value */
-	int client_sock[MAX_CLIENTS];
-	int cnt;  /* count of sockets of interest at an invocation */
-	int status;
+  while ((opt = getopt(argc, argv, "i:p:")) != -1) {
+    switch (opt) {
+    case 'i':
+      ip_addr = optarg;
+      break;
 
-	/* initialize the client socket list */
-	for  (int ii = 0; ii < MAX_CLIENTS; ii++) {
-		client_sock[ii] = 0;
-	}
-	FD_ZERO(&rset); FD_ZERO(&wset);
+    case 'p':
+      port = atoi(optarg);
+      break;
 
-	bzero((void *)&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr(ip_addr);
-	servaddr.sin_port = htons(port);
+    default:
+      fprintf(stderr, "Usage: %s -i ipaddr -p port\n", argv[0]);
+      exit(1);
+      break;
+    } /* switch(opt) */
+  } /* while (opt...) */
 
-	listenfd = socket (AF_INET, SOCK_STREAM, 0);
-	bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-	listen(listenfd, LISTENQ);
+  if ((port == 0) || (ip_addr == (char *)NULL)) {
+    fprintf(stderr, "error in  paddr or port number value\n");
+    exit(1);
+  }
 
-	/* need to make listenfd as part of rset as it provides new connection */
-	FD_SET(listenfd, &rset);
-	max_fd = listenfd + 1;
+  /* initialize the client socket list */
+  for  (ii = 0; ii < MAX_CLIENTS; ii++) {
+    client_sock[ii] = 0;
+  }
+  FD_ZERO(&rset);
+  FD_ZERO(&wset);
 
-	clilen = sizeof(cliaddr);
-	for (; ; ) {
-		/* setup the working set */
-		FD_ZERO(&wk_rset);
-		FD_ZERO(&wk_wset);
-		for (ii=0; ii < max_fd; ii++) {
-			if (FD_ISSET(ii, &rset)) {
-				FD_SET(ii, &wk_rset);
-			}
-		}
-		/* set some timeout value */
-		timeout.tv_sec = SELECT_TIMEOUT;
-		timeout.tv_usec = 0;
+  listenfd = socket (AF_INET, SOCK_STREAM, 0);
 
-		cnt = select(max_fd, &wk_rset, &wk_wset, NULL, &timeout);
-    if (cnt == 0) {
-			printf("Select timeout occurred for timeout of %ds\n", SELECT_TIMEOUT);
-			continue;
-		}
-		/* some sockets are of interest */
-		/* check if new connection has arrived */
-		if (FD_ISSET(listenfd, &wk_rset)) {
-			connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
-			/* new connection is to be made part of master set */
-      ntohs(cliaddr.sin_port));
-			client_sock[connfd] = connfd; /* any true value */
-			max_fd = connfd + 1;
+  bzero((void *)&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = inet_addr(ip_addr);
+  servaddr.sin_port = htons(port);
 
-			FD_SET(connfd, &rset);
-			FD_SET(connfd, &wset);
-		}
+  bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+  listen(listenfd, LISTENQ);
 
-		/** the main processing of server sockets
-		 * for each socket which is returned by select, read the data and respond back
-		 */
-		 for (int ifd = 0; ifd < max_fd; ifd++ ) {
-		 	if (ifd == listenfd) {
-				continue;
-			}
-			if (FD_ISSET(ifd, &wk_rset)) {
-				status = str_echo(ifd);
-				if ((status <= 0) && (errno != EINTR)) {
-					FD_CLR(ifd, &rset);
-					FD_CLR(ifd, &wset);
-					close(ifd);
-				}
-			}
-		}
+  /* need to make listenfd as part of rset as it provides new connection */
+  FD_SET(listenfd, &rset);
+  max_fd = listenfd + 1;
+  
+  clilen = sizeof(cliaddr);
+  for (; ; ) {
+    /* setup the working set */
+    FD_ZERO(&wk_rset);
+    FD_ZERO(&wk_wset);
+    for (ii=0; ii < max_fd; ii++) {
+      if (FD_ISSET(ii, &rset)) {
+        FD_SET(ii, &wk_rset);
+      }
+    }
+    /* set some timeout value */
+    timeout.tv_sec = SELECT_TIMEOUT;
+    timeout.tv_usec = 0;
 
-	}
-}
+    cnt = select(max_fd, &wk_rset, &wk_wset, NULL, &timeout);
+    if (cnt == -1) {
+      fprintf(stderr, "Error %d in select()", errno);
+      exit(1);
+    } else if (cnt == 0) {
+      printf("Select timeout occurred for timeout of %ds\n", SELECT_TIMEOUT);
+      continue;
+    }
+    /* some sockets are of interest */
+    /* check if new connection has arrived */
+    if (FD_ISSET(listenfd, &wk_rset)) {
+      connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
+      if (connfd > MAX_CLIENTS) {
+        fprintf(stderr, "new connection from client %s exceeds server capacity\n", inet_ntoa(cliaddr.sin_addr));
+        close(connfd); /* beyond capacity of server */
+        continue;
+      }
+      /* new connection is to be made part of master set */
+      printf("new connection from client %s:%d accepted\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
+      client_sock[connfd] = connfd; /* any true value */
+      if (max_fd < connfd + 1) {
+        max_fd = connfd + 1;
+      }
+      FD_SET(connfd, &rset);
+      FD_SET(connfd, &wset);
+    }
+
+    /** the main processing of server sockets 
+     * for each socket which is returned by select, read the data and respond back
+     */
+     for (ifd = 0; ifd < max_fd; ifd++ ) {
+       if (ifd == listenfd) {
+        continue;
+      }
+      if (FD_ISSET(ifd, &wk_rset)) {
+        status = str_echo(ifd);
+        if ((status <= 0) && (errno != EINTR)) {
+          FD_CLR(ifd, &rset);
+          FD_CLR(ifd, &wset);
+          close(ifd);
+        } /* if status <=0 */
+      } /* if FD_ISSET */
+    } /* for ifd = 0; */
+  } /* for (; ; ) */
+} /* main */
 
 /* just do one read. For subsequent read, this function will be invoked again */
 int str_echo(int clientfd) {
-	char buf[BUFFSIZE];
-	int size;
+  char buf[BUFFSIZE];
+  int size;
 
-	bzero(buf, sizeof(buf));
-	size = recv(clientfd, buf, sizeof(buf), 0);
-	send(clientfd, buf, strnlen(buf, sizeof(buf)), 0);
+  bzero(buf, sizeof(buf));
+  size = recv(clientfd, buf, sizeof(buf), 0);
+  send(clientfd, buf, strnlen(buf, sizeof(buf)), 0);
 }
